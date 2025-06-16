b036b7d517b43fc72711372de170855d6cdf1d57
Bubsa <bube.sim@gmail.com>
2025-06-16 17:38:59 +0200

    Class 03

---
+++ Qinshift.Class03/Qinshift.Models/Controllers/StudentController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Qinshift.Models.Services;
+
+namespace Qinshift.Models.Controllers
+{
+    //[Route("students")] // custom route name for our controller 
+    [Route("[controller]/[action]")] // set routing to be "controllerName/actionName"
+    public class StudentController : Controller
+    {
+        private readonly StudentService _studentService;
+
+        public StudentController()
+        {
+            _studentService = new StudentService();
+        }
+
+        //[Route("all")] // /students/all (if using Route("students") for the controller)
+        [HttpGet] // /student/getallstudents
+        public IActionResult GetAllStudents()
+        {
+            var students = _studentService.GetAllStudents();
+
+            //return View(students);
+            // NOTE: Handling the ViewModels in the View, as well as creating Views will be discussed in the following lectures
+            return Json(students);
+        }
+
+        [HttpGet("{id}")] // /student/GetStudentById/3
+        public IActionResult GetStudentById(int id)
+        {
+            var student = _studentService.GetStudentWithActiveCourseById(id);
+
+            if (student is null)
+            {
+                return Content("Student not found!");
+            }
+
+            return Json(student);
+        }
+
+        // EXERCISE 01
+        [HttpGet("{id}")]
+        public IActionResult GetStudentCourseDetails(int id)
+        {
+            var studentCourseDetails = _studentService.GetStudentCourseDetail(id);
+
+            if (studentCourseDetails is null)
+            {
+                return Content("No user found!");
+            }
+
+            return Json(studentCourseDetails);
+        }
+    }
+}
---
+++ Qinshift.Class03/Qinshift.Models/Services/StudentService.cs
@@ -0,0 +1,81 @@
+﻿using Qinshift.Models.Database;
+using Qinshift.Models.Models.DomainModels;
+using Qinshift.Models.Models.DtoModels;
+using Qinshift.Models.Models.ViewModels;
+
+namespace Qinshift.Models.Services
+{
+    public class StudentService
+    {
+        public List<StudentViewModel> GetAllStudents()
+        {
+            // 1. Get students from DB
+            List<Student> studentsDb = InMemoryDb.Students;
+
+            // 2. Validate 
+            if (!studentsDb.Any())
+            {
+                return new List<StudentViewModel>();
+            }
+
+            // 3. Map (transform) the Domain model objects to ViewModel objects that contain only the necessery information
+            List<StudentViewModel> mappedStudents = studentsDb
+                .Select(s => new StudentViewModel
+                {
+                    Id = s.Id,
+                    FullName = $"{s.FirstName} {s.LastName}",
+                    Course = s.ActiveCourse.Name
+                })
+                .ToList();
+
+            // 4. Return the mapped list 
+            return mappedStudents;
+        }
+
+        public StudentWithCourseDto GetStudentWithActiveCourseById(int studentId)
+        {
+            // 1. Get student by id from db
+            Student student = InMemoryDb.Students.FirstOrDefault(s => s.Id == studentId);
+
+            // 2. Validate if student is null
+            if (student is null)
+            {
+                return null;
+            }
+
+            // 3. Map (transform) the Domain model into Dto model containing only the needed information
+            StudentWithCourseDto studentDto = new StudentWithCourseDto
+            {
+                Id = student.Id,
+                FullName = $"{student.FirstName} {student.LastName}",
+                Age = DateTime.Now.Year - student.DateOfBirth.Year,
+                CourseName = student.ActiveCourse.Name
+            };
+
+            // 4. Return the mapped Dto model
+            return studentDto;
+        }
+
+        // EXERCISE 01
+        public StudentCourseDetailDto? GetStudentCourseDetail(int id)
+        {
+            var studentDb = InMemoryDb.Students.FirstOrDefault(s => s.Id == id);
+
+            if (studentDb == null)
+            {
+                return null;
+            }
+
+            var studentCourseDto = new StudentCourseDetailDto
+            {
+                StudentId = studentDb.Id,
+                FullName = studentDb.FirstName + " " + studentDb.LastName,
+                CourseId = studentDb.ActiveCourse.Id,
+                CourseName = studentDb.ActiveCourse.Name,
+                NumberOfClasses = studentDb.ActiveCourse.NumberOfClasses,
+            };
+
+            return studentCourseDto;
+        }
+    }
+}
